633ed273238729622ce524471c94e4ac1fa5ed6b
sametweb <sametmutevellioglu@gmail.com>
2021-05-06 01:22:55 -0700

    Helmet and Cors added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const server = express();
 
 const typeRouter = require("./routers/typeRouter");
@@ -6,9 +9,15 @@ const categoryRouter = require("./routers/categoryRouter");
 const recordRouter = require("./routers/recordRouter");
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 server.use("/types", typeRouter);
 server.use("/categories", categoryRouter);
 server.use("/records", recordRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json(req.headers);
+});
 
 const PORT = process.env.PORT || 5000;
 
