c2e347f91cdc379f046a6be4180ff8cbf8b76509
sametweb <sametmutevellioglu@gmail.com>
2021-07-03 18:36:57 -0700

    Break register if username or email exists

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,23 +7,22 @@ const generateToken = require("../utils/generateToken");
 router.post("/register", async (req, res, next) => {
   const userData = req.body;
   const rounds = process.env.HASH_ROUNDS || 12;
-  console.log({ userData });
 
   if (!userData.username || !userData.password || !userData.email) {
-    next([400, "Username and email are required."]);
+    return next([400, "Username and email are required."]);
   }
 
   if (userData.password && userData.password.length < 6) {
-    next([400, "Password must be at least 6 characters."]);
+    return next([400, "Password must be at least 6 characters."]);
   }
 
   userData.password = bcrypt.hashSync(userData.password, rounds);
 
   try {
     const uExists = await User.findByUsername(userData.username);
-    if (uExists) next([400, "Username is used by someone else"]);
+    if (uExists) return next([400, "Username is used by someone else"]);
     const eExists = await User.findByEmail(userData.email);
-    if (eExists) next([400, "Email is used by someone else"]);
+    if (eExists) return next([400, "Email is used by someone else"]);
     const added = await User.addUser(userData);
     res.status(201).json(added);
   } catch {
@@ -35,7 +34,8 @@ router.post("/login", async (req, res, next) => {
   const userData = req.body;
   const { username, password } = userData;
 
-  if (password.length < 6) next([400, "Password must be at least 6 characters."]);
+  if (password.length < 6)
+    return next([400, "Password must be at least 6 characters."]);
 
   try {
     const user = await User.findByUsername(username);
